fda0d0b5649cf3b9aa661bbf4b415c312defb970
Jiaying Huang <jiaying.huang-4@student.manchester.ac.uk>
2022-10-21 19:13:07 +0100

    Spring boot Maven Project Setup

---
+++ personal-blog/src/test/java/com/jiaying/PersonalBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiaying;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
