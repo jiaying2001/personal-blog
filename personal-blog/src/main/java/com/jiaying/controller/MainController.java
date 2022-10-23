91d375c5b872298feaae9722c6e64880e1855356
Jiaying Huang <jiaying.huang-4@student.manchester.ac.uk>
2022-10-23 22:21:09 +0100

    Set up basic files for a simple personal blog website

---
+++ personal-blog/src/main/java/com/jiaying/controller/MainController.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.jiaying.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author jiayinghuang
+ *
+ */
+
+@Slf4j
+@Controller
+@RequestMapping("/")
+public class MainController {
+	@GetMapping
+	public String showHome() {
+		return "home";
+	}
+	
+}
